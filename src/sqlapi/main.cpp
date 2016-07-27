b98229cb760434fdf1951e2d1c4e16341e128032
attackercan <vi11@student.london.ac.uk>
2016-07-27 19:56:31 +0300

    Finishing cpp with SQLAPI

--- src/sqlapi/main.cpp
+++ src/sqlapi/main.cpp
@@ -1,28 +1,114 @@
-#include <cstdlib>
-#include <stdio.h>
 #include <iostream>
-
+#include <cstdio>
+#include <cstdlib>
+#include <string.h>
+#include <vector>
+#include <algorithm>
+#include <sstream>
 #include <SQLAPI.h>
 
-bool checkSQL(SAString text) {
+using namespace std;
 
-    SAConnection con; // create connection object
-    SACommand cmd;
+#define CONNSTRING "localhost@test" // server@dbname
+#define DBUSER "root" // dbuser
+#define DBPASS "" // dbpass
 
+char* fillArray(char Alphabet[])
+{
+
+    for (int i = 32; i <= 96; i++) {
+        char a = i;
+
+        Alphabet[i - 32] = a;
+    }
+
+    int i = 123; // {
+    char a = i;
+    Alphabet[65] = a;
+
+    i = 124; // |
+    a = i;
+    Alphabet[66] = a;
+
+    i = 125; // }
+    a = i;
+    Alphabet[67] = a;
+
+    i = 126; // ~
+    a = i;
+    Alphabet[27] = a; // removing ; because of SQLAPI
+}
+
+bool replace(std::string& str, const std::string& from, const std::string& to)
+{
+    size_t start_pos = str.find(from);
+    if (start_pos == std::string::npos)
+        return false;
+
+    // replace all substrings ?
+    while ((start_pos = str.find(from, start_pos)) != std::string::npos) {
+        str.replace(start_pos, from.length(), to);
+        start_pos += to.length();
+    }
+
+    return true;
+}
+
+std::string parse_libinjection(std::string text)
+{
+    char cmd[500];
+    string libInjInput = text;
+
+    // TODO: better bash escaping
+    replace(libInjInput, "'", "'\\''");
+
+    strcpy(cmd, "./detect '");
+    strcat(cmd, libInjInput.c_str());
+    strcat(cmd, "'");
+    char buf[128];
+    FILE* fp;
+
+    if ((fp = popen(cmd, "r")) == NULL) {
+        printf("Error opening file!\n");
+        return "";
+    }
+
+    fgets(buf, 128, fp);
+    return buf;
+}
+
+string dec2hex(unsigned long long i)
+{
+    stringstream ss;
+    ss << hex << uppercase << i;
+    return ss.str();
+}
+
+void StringExplode(string str, string separator, vector<string>* results)
+{
+    int found;
+    found = str.find_first_of(separator);
+    while (found != string::npos) {
+        if (found > 0) {
+            results->push_back(str.substr(0, found));
+        }
+        str = str.substr(found + 1);
+        found = str.find_first_of(separator);
+    }
+    if (str.length() > 0) {
+        results->push_back(str);
+    }
+}
+
+bool checkSQL(SAConnection &con, SACommand &cmd, SAString text) {
     try {
-        con.Connect(
-            "test", // database name
-            "root", // user name
-            "", // password
-            SA_MySQL_Client);
-        cmd.setConnection(&con);
-
         cmd.setCommandText(text);
         cmd.Execute();
-        //bool is_result = cmd.isResultSet();
-        if(cmd.isResultSet()) return true;
+        if(cmd.isResultSet())
+	    return true;
     }
-    catch (SAException) {
+    catch (SAException &x) {
+	//printf("%s\n", (const char*)x.ErrText());
         return false;
     }
 }
@@ -30,8 +116,91 @@ bool checkSQL(SAString text) {
 
 int main(int argc, char* argv[])
 {
-    bool result = checkSQL("select 1");
-    printf("%d", result);
+    string sql_input;
+    if (argc > 1)
+    {
+    	sql_input = string(argv[1]);
+    } else
+    {
+    	cout << "Please input fuzzing string with [XXX] input, e.g. ./mysql_fuzz.out 'SELECT[XXX]1 FROM tbl1'\r\n";
+	return 0;
+    }
+
+    SAConnection con;
+    SACommand cmd;
+
+    con.Connect(CONNSTRING, DBUSER, DBPASS, SA_MySQL_Client); //SA_Oracle_Client, etc
+    cmd.setConnection(&con);
+
+    char Alphabet[68];
+    fillArray(Alphabet);
+
+    int good = 0;
+
+    for (int i = 0; i <= sizeof(Alphabet); i++) {
+        for (int j = 0; j <= sizeof(Alphabet); j++) {
+            for (int k = 0; k <= sizeof(Alphabet); k++) {
+                // for(int l = 0; l <= sizeof(Alphabet); l++) {
+
+                string FinalFuzzWord = "";
+                FinalFuzzWord += Alphabet[i];
+                FinalFuzzWord += Alphabet[j];
+                FinalFuzzWord += Alphabet[k];
+                // FinalFuzzWord += Alphabet[l];
+
+                char tmpc1 = Alphabet[i];
+                int tmpi = tmpc1;
+                char tmpc2 = Alphabet[j];
+                int tmpj = tmpc2;
+                char tmpc3 = Alphabet[k];
+                int tmpk = tmpc3;
+                // char tmpc4 = Alphabet[l]; int tmpl = tmpc4;
+
+                string FinalFuzzWordUnicode = "";
+                FinalFuzzWordUnicode += "%" + dec2hex(tmpi);
+                FinalFuzzWordUnicode += "%" + dec2hex(tmpj);
+                FinalFuzzWordUnicode += "%" + dec2hex(tmpk);
+                // FinalFuzzWordUnicode += "%" + dec2hex(tmpl);
+
+                string sql = sql_input;
+                string sql_unicode = sql;
+
+                replace(sql, "[XXX]", FinalFuzzWord);
+                replace(sql_unicode, "[XXX]", FinalFuzzWordUnicode);
+
+		bool querySuccessfull = checkSQL(con, cmd, SAString(sql.c_str()));
+
+                if (querySuccessfull == true) {
+                    char* escaped_sql = new char[strlen(sql.c_str()) * 3 + 1];
+                    char* escaped_sql_unicode = new char[strlen(sql_unicode.c_str()) * 3 + 1];
+
+                    string escaped_sql_string = string(sql);
+                    string escaped_sql_unicode_string = string(sql_unicode);
+
+                    vector<string> R;
+                    string thisstring = parse_libinjection(escaped_sql_string);
+                    StringExplode(thisstring, ":", &R);
+
+                    string sql_add = "INSERT INTO good (id, vector, unicode_vector, "
+                                     "libinj_token, libinj_isSQLi) VALUES (NULL, '"
+                        + escaped_sql_string + "', '" + escaped_sql_unicode_string + "', '" + R[0] + "', '" + R[1] + "');";
+		    
+		    try {
+			cmd.setCommandText(sql_add.c_str(), SA_CmdSQLStmt); // setCommandText doesn't accept some symbols, i.e. %0, ; and hence skips some payloads. This is a payoff to compatibility (mysql, sqlserver, oracle, etc...)
+			cmd.Execute();
+		    }
+		    catch (SAException &f) {printf("Cant insert >> %s\n", escaped_sql_unicode_string.c_str());}
+                    good++;
+                }
+                //}
+            }
+        }
+    }
+
+    cout << "\nGOOD: ";
+    cout << good;
+    cout << "\n";
+    con.Disconnect();
 
     return 0;
 }
